12b4a992d9e4ef82e5670bc7e873ca4e94794aae
mynamedust <mynamedust@gmail.com>
2023-11-12 21:02:24 +0400

    update migration

--- devops/migrations/000002_create_product_table.up.sql
+++ devops/migrations/000002_create_product_table.up.sql
@@ -13,17 +13,17 @@ ALTER TABLE product
     ADD CONSTRAINT positive_reserved_constraint CHECK (reserved >= 0);
 ALTER TABLE product
     ADD CONSTRAINT unique_code_per_stock UNIQUE (stock_id, code);
-INSERT INTO product (name, size, code, quantity, stock_id)
+INSERT INTO product (name, size, code, quantity, reserved, stock_id)
 VALUES
-    ('Product 1', 'Small', 'P0001', 100, 1),
-    ('Product 2', 'Medium', 'P0002', 150, 1),
-    ('Product 3', 'Large', 'P0003', 200, 2),
-    ('Product 4', 'Small', 'P0004', 75, 2),
-    ('Product 5', 'Medium', 'P0005', 120, 1),
-    ('Product 6', 'Large', 'P0006', 180, 2),
-    ('Product 7', 'Small', 'P0007', 90, 2),
-    ('Product 8', 'Medium', 'P0008', 110, 1),
-    ('Product 9', 'Large', 'P0009', 160, 1),
-    ('Product 10', 'Small', 'P0010', 50, 1),
-    ('Product 11', 'Medium', 'P0011', 70, 2),
-    ('Product 12', 'Large', 'P0012', 220, 1);
+    ('Product 1', 'Small', 'P0001', 100, 0,1),
+    ('Product 2', 'Medium', 'P0002', 150, 0,1),
+    ('Product 3', 'Large', 'P0003', 200, 0,2),
+    ('Product 4', 'Small', 'P0004', 75, 0,2),
+    ('Product 5', 'Medium', 'P0005', 120, 4, 1),
+    ('Product 6', 'Large', 'P0006', 180, 20,2),
+    ('Product 7', 'Small', 'P0007', 90, 5, 2),
+    ('Product 8', 'Medium', 'P0008', 110, 0, 1),
+    ('Product 9', 'Large', 'P0009', 160, 0, 1),
+    ('Product 10', 'Small', 'P0010', 50, 0, 1),
+    ('Product 11', 'Medium', 'P0011', 70, 0, 2),
+    ('Product 12', 'Large', 'P0012', 220, 15, 1);
